a348729af19b28cdb701ca589f57e3fe98f8a5c2
Jatin Kumar <jk031997@gmail.com>
2020-07-15 19:01:19 +0530

    version-1

---
+++ Client/build/precache-manifest.971b0f1bf68f12d5c8f029a302894e39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201515e3f2752c7d676254be0d0ba847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3e975dd12fe01ef78e9",
+    "url": "/static/css/main.994e1518.chunk.css"
+  },
+  {
+    "revision": "33f6a9c78834e5f0374f",
+    "url": "/static/js/2.3e40adf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e40adf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e975dd12fe01ef78e9",
+    "url": "/static/js/main.c306a0d6.chunk.js"
+  },
+  {
+    "revision": "522f7dc68da50ab58d85",
+    "url": "/static/js/runtime-main.590e9b37.js"
+  }
+]);
