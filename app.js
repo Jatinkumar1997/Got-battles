9c7767d69f8d0293234a1563360c1166af04eaac
Jatin Kumar <jk031997@gmail.com>
2020-07-18 04:41:32 +0530

    some fixes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 
 const app = express();
 const port = process.env.PORT || 8080
-const mongoURI = process.env.MONGODB_URI || "mongodb+srv://jatin19:jatin1903@cluster0.mwozi.mongodb.net/TestDB?retryWrites=true&w=majority"
+const mongoURI = process.env.MONGODB_URI || "mongodb+srv://<username>:<password>@cluster0.mwozi.mongodb.net/<DBName>?retryWrites=true&w=majority"
 
 mongoose.connect(mongoURI,{useNewUrlParser: true, useUnifiedTopology: true}).catch(err=>console.log(err))
 const db = mongoose.connection
